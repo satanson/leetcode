868279f8e062445ac38b8b7ab19c76e11082bb2d
satanson <ranpanf@gmail.com>
2014-07-22 14:16:03 +0800

    33.issue: time complexity is O(n^2),but pass

--- src/33_Pascal_TriangleII.cpp
+++ src/33_Pascal_TriangleII.cpp
@@ -1,9 +1,9 @@
 #include<cppstdlib.hpp>
 class Solution {
 public:
-    vector<vector<int>> generate(int numRows) {
-		vector<vector<int>> tri(numRows,vector<int>());
-		if (tri.size()==0)return tri;
+    vector<int> getRow(int rowIndex) {
+		vector<vector<int>> tri(rowIndex+1,vector<int>());
+		if (tri.size()==0)return vector<int>();
 		tri[0].push_back(1);
 		for (int i=1;i<tri.size();i++){
 			vector<int> row(i+1);
@@ -14,24 +14,21 @@ public:
 			}
 			tri[i][tri[i].size()-1]=1;
 		}
-		return tri;
+		return tri[tri.size()-1];
     }
 };
 
-void print (vector<vector<int>>& tri){
+void print (vector<int>& row){
 	cout<<string(20,'#')<<endl;
-	for(int i=0;i<tri.size();i++){
-		for(int j=0;j<tri[i].size();j++){
-			cout<<tri[i][j]<<"\t";
-		}
-		cout<<endl;
+	for(int i=0;i<row.size();i++){
+		cout<<row[i]<<"\t";
 	}
-	cout<<string(20,'#')<<endl;
+	cout<<endl<<string(20,'#')<<endl;
 }
 int main(){
 	int n;
 	cin>>n;
 	Solution so;
-	vector<vector<int>> tri=so.generate(n);
-	print(tri);
+	vector<int> row=so.getRow(n);
+	print(row);
 }
