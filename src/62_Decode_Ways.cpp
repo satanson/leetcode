4fb9b00b5c3abdbeb72225ac6b15f6e1ebc791b4
satanson <ranpanf@gmail.com>
2014-07-28 21:44:25 +0800

    by satanson

--- src/62_Decode_Ways.cpp
+++ src/62_Decode_Ways.cpp
@@ -2,14 +2,18 @@
 class Solution{
 public:
 	int numDecodings(string s){
-		if (s.size()==0)return 1;
+		if(s.size()==0)return 0;
 		vector<int> a(s.size()+1);
-		a[0]=1;a[1]=1;
+		a[0]=1;
+		if (s[0]=='0')a[1]=0;
+		else a[1]=1;
+
 		for (int i=2;i<a.size();i++){
-			if(s[i-2]>'2' || s[i-2]=='2' && s[i-1]>'6'){
-				a[i]=a[i-1];
-			}else{
-				a[i]=a[i-1]+a[i-2];
+			if(s[i-2]=='1' || s[i-2]=='2' && s[i-1]<='6'){
+				a[i]+=a[i-2];
+			}
+			if (s[i-1]!='0'){
+				a[i]+=a[i-1];
 			}
 		}
 		return a[s.size()];
