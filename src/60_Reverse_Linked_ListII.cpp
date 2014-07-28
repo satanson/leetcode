4d32509c9577b82351e51267394e04ec433d3b5b
satanson <ranpanf@gmail.com>
2014-07-28 19:01:57 +0800

    60.issue: segmentation fault when perform second test

---
+++ src/60_Reverse_Linked_ListII.cpp
@@ -0,0 +1,37 @@
+#include<list_serialize.hpp>
+class Solution {
+public:
+	ListNode *reverseBetween(ListNode *head,int m,int n){
+		ListNode *p=new ListNode(0);
+		p->next=head;
+		head=p;
+
+		int k=0;
+		while(++k<m) p=p->next;
+
+		ListNode *q=NULL,*h=NULL,*t=NULL;
+		k=0;
+		q=p->next;
+		while(k++<n-m+1){
+			t=q->next;
+			q->next=h;
+			h=q;
+			q=t;
+		}
+
+		p->next->next=t;
+		p->next=h;
+		p=head->next;
+		delete head;
+		return p;
+	}
+};
+int main(){
+	Solution so;
+	string line;
+	while(getline(cin,line)){
+		int m,n;
+		cin>>m>>n;
+		cout<<list2string(so.reverseBetween(string2list(line),m,n))<<endl;
+	}
+}
