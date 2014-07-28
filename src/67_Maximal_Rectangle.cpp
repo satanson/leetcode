7cbcfd94c54d8406b523c850c2a2f76e6d06672e
satanson <ranpanf@gmail.com>
2014-07-29 04:38:44 +0800

    67.issue: axises of subrectangle rightbottom corner is wrong

--- src/67_Maximal_Rectangle.cpp
+++ src/67_Maximal_Rectangle.cpp
@@ -3,63 +3,36 @@ class Solution{
 public:
 	int maximalRectangle(vector<vector<char>> &matrix){
 		if (matrix.size()==0||matrix[0].size()==0)return 0;
-		int W=matrix.size();
-		int H=matrix[0].size();
-
-		vector<vector<vector<vector<bool>>>> d(
-				W,vector<vector<vector<bool>>>(
-					H,vector<vector<bool>>(
-						W+1,vector<bool>(H+1))));
-
-		int area=0;
-		for (int left=0;left<W;left++){
-			for (int top=0;top<H;top++){
-				if (matrix[left][top]=='1'){
-					d[left][top][1][1]=true;
-					area=1;
-				} else d[left][top][1][1]=false;
+		return maxArea(matrix,0,0,matrix.size(),matrix[0].size());
+	}
+	int maxArea(vector<vector<char>> &matrix,int left,int top,int right,int bottom){
+		int width=right-left;
+		int height=bottom-top;
+		if (width<=0 || height<=0) return 0;
+		if (width==1 && height==1 && matrix[left][top]=='1')return 1;
+		int x=0,y=0;
+		for ( x=left;x<right;x++){
+			for (y=top;y<bottom;y++){
+				if (matrix[x][y]=='0')goto LOOP;
 			}
 		}
-
-		for (int w=2;w<=W;w++){
-			for (int x=0;x+w<=W;x++){
-				for (int y=0;y<H;y++){
-					if (d[x][y][1][1] && d[x+1][y][w-1][1]){
-						d[x][y][w][1]=true;
-						if (area<w)area=w;
-					} else d[x][y][w][1]=false;
-				}
-			}
+LOOP:
+		if (x==right && y==bottom){
+			cout<<"("<<left<<","<<top<<");"
+				<<"("<<right<<","<<bottom<<")"<<endl;
+			return width*height;
+		} else{
+			int area=0;
+			int subarea=maxArea(matrix,left,top,x-1,y-1);
+			if (area<subarea)area=subarea;
+			subarea=maxArea(matrix,left,y+1,x-1,bottom);
+			if (area<subarea)area=subarea;
+			subarea=maxArea(matrix,x+1,top,right,y-1);
+			if (area<subarea)area=subarea;
+			subarea=maxArea(matrix,x+1,y+1,right,bottom);
+			if (area<subarea)area=subarea;
+			return area;
 		}
-		for (int h=2;h<=H;h++){
-			for (int x=0;x<W;x++){
-				for (int y=0;y+h<=H;y++){
-					if (d[x][y][1][1] && d[x][y+1][1][h-1]){
-						d[x][y][1][h]=true;
-						if (area<h)area=h;
-					} else d[x][y][1][h]=false;
-				}
-			}
-		}
-
-		for (int w=2;w<=W;w++){
-			for (int h=2;h<=H;h++){
-				for (int x=0;x+w<=W;x++){
-					for (int y=0;y+h<=H;y++){
-						if (d[x][y][1][1]&&
-								d[x+1][y][w-1][h]&&
-								d[x][y+1][w][h-1]&&
-								d[x+1][y+1][w-1][h-1]){
-
-							d[x][y][w][h]=true;
-							int area0=w*h;
-							if (area<area0)area=area0;
-						}else d[x][y][w][h]=false;
-					}
-				}
-			}
-		}
-		return area;
 	}
 
 };
