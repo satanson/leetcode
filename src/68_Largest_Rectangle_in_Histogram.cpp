34ed8011074ad739a076d39e8ed7bb032fa55b36
satanson <ranpanf@gmail.com>
2014-07-29 18:48:26 +0800

    68.issue: wrong answer

--- src/68_Largest_Rectangle_in_Histogram.cpp
+++ src/68_Largest_Rectangle_in_Histogram.cpp
@@ -2,28 +2,22 @@
 #include<list_serialize.hpp>
 class Solution {
 public:
+	typedef pair<int,int> PAIR;
 	int largestRectangleArea(vector<int> &height){
-		vector<int> stack;
-		stack.reserve(height.size());
+		vector<PAIR> stack;
+		stack.reserve(height.size()+1);
+		stack.push_back(PAIR(0,-1));
 		int maxArea=0;
-		for(int i=0;i<height.size();i++){
-			while (!stack.empty() && height[stack.back()]>=height[i]){
-					int j=stack.back();stack.pop_back();
-					int width=0;
-					if (stack.empty())width=i;
-					else width=i-stack.back()-1;
-					int area=height[j]*width;
-					if (area>maxArea)maxArea=area;
+		for(int i=0;i<=height.size();i++){
+			int h=height[i];
+			if (i==height.size())h=0;
+
+			while(stack.size()>1 && stack.back().first>=h){
+				PAIR p=stack.back();stack.pop_back();
+				int area=h*(i-stack.back().second-1);
+				if (area>maxArea)maxArea=area;
 			}
-			stack.push_back(i);
-		}
-		while(!stack.empty()){
-			int j=stack.back();stack.pop_back();
-			int width=0;
-			if (stack.empty())width=height.size();
-			else width=height.size()-stack.back()-1;
-			int area=height[j]*width;
-			if (area>maxArea)maxArea=area;
+			stack.push_back(PAIR(h,i));
 		}
 		return maxArea;
 	}
