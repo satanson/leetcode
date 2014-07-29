47fbfe52d67986aacf79ab21c44051eb07bbbc7f
satanson <ranpanf@gmail.com>
2014-07-29 23:18:09 +0800

    25.issue: use int instead string to compute paths

--- src/25_Word_LadderII_Timeout.cpp
+++ src/25_Word_LadderII_Timeout.cpp
@@ -2,28 +2,16 @@
 class Solution {
 public:
     vector<vector<string>> findLadders(string start, string end, unordered_set<string> &dict) {
-		vector<string> t;
-		unordered_map<string,int> m;
-		t.reserve(dict.size()+2);
-		t.push_back(start);
-		t.push_back(end);
-		for (auto e=dict.begin();e!=dict.end();e++){
-			string w=*e;
-			t.push_back(w);
-			m[w]=t.size()-1;
-		}
-
-		list<list<int>> q1,q2,q;
+		vector<vector<string>> q1,q2;
 		list<string> junk;
-
 		vector<vector<string>> ladders;
-		q1.push_back(list<int>());q1.back().push_back(0);
+		q1.push_back(vector<string>());q1.back().push_back(start);
 		dict.insert(end);
-		while(!q1.empty() ){
+		while(!q1.empty() && dict.size()){
 			while(!q1.empty()){
 
-				list<int> path;std::swap(q1.back(),path);q1.pop_back();
-				string& s=t[path.back()];
+				vector<string> path;std::swap(q1.back(),path);q1.pop_back();
+				string& s=path.back();
 				for (int i=0;i<s.size();i++){
 					string s1=s;
 					for (char c='a';c<='z';c++){
@@ -31,12 +19,13 @@ public:
 						if (c==s[i])continue;
 						if (s1==end){
 							if(ladders.empty()||ladders.back().size()==path.size()+1){
-								q.push_back(path);q.back().push_back(1);
+								ladders.push_back(vector<string>(path.begin(),path.end()));
+								ladders.back().push_back(s1);
 							}else continue;
 						}
 
 						if (dict.count(s1)>0){
-							q2.push_back(path);q2.back().push_back(m[s1]);
+							q2.push_back(path);q2.back().push_back(s1);
 							junk.push_back(s1);
 						}
 					}
@@ -44,12 +33,7 @@ public:
 			}
 			while(!junk.empty()){dict.erase(junk.back());junk.pop_back();}
 			std::swap(q1,q2);
-			if (!q.empty()) break;
-		}
-		while(!q.empty()){
-			list<int> path;std::swap(path,q.back());q.pop_back();
-			ladders.push_back(vector<string>());
-			while(!path.empty()){ladders.back().push_back(t[path.front()]);path.pop_front();}
+			if (!ladders.empty()) return ladders;
 		}
 		return ladders;
 	}
--- src/26_Word_Ladder.cpp
+++ src/26_Word_Ladder.cpp
@@ -2,18 +2,13 @@
 class Solution {
 public:
     int ladderLength(string start, string end, unordered_set<string> &dict) {
-		list<list<int>> q1,q2;
-		vector<string> v;
-		v.reserve(dict.size()+2);
-		v.push_back(start);
-		v.push_back(end);
-		dict.insert(end);
-
-		q1.push_back(list<int>());q1.back().push_back(0);
-		while(!q1.empty() &&!dict.empty()){
+		list<list<string>> q1,q2;
+		//dict.insert(end);
+		q1.push_back(list<string>());q1.back().push_back(start);
+		while(!q1.empty()){
 			while(!q1.empty()){
-				list<int> path;std::swap(q1.back(),path);q1.pop_back();
-				string& s1=v[path.back()];
+				list<string> path;std::swap(q1.back(),path);q1.pop_back();
+				string s1=path.back();
 				for (int i=0;i<s1.size();i++){
 					char c0=s1[i];
 					for (char c='a';c<='z';c++){
@@ -22,8 +17,7 @@ public:
 						if (s1==end)return path.size()+1;
 
 						if (dict.count(s1)>0){
-							v.push_back(s1);
-							q2.push_back(path);q2.back().push_back(v.size()-1);
+							q2.push_back(path);q2.back().push_back(s1);
 							dict.erase(s1);
 						}
 					}
