c810394a53abc91321f1503ca7cfeff08d660a99
satanson <ranpanf@gmail.com>
2014-07-30 20:49:10 +0800

    28.issue:INT_MIN>0 is true,http://stackoverflow.com/questions/14695118/2147483648-0-returns-true-in-c

--- src/28_Binary_Tree_Maximum_Path_Sum.cpp
+++ src/28_Binary_Tree_Maximum_Path_Sum.cpp
@@ -37,6 +37,8 @@ public:
 			aa=branch->val;
 			if (b[0]>0)aa+=b[0];
 			if (b[1]>0)aa+=b[1];
+		}else if (b[0]>0 && b[0]>0 && b[0]+b[1]+branch->val>bb){
+			aa=b[0]+b[1]+branch->val;
 		}else{
 			aa=bb;
 		}
