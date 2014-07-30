3fe1a846227fae7cd23e7c2518f28116444301a1
satanson <ranpanf@gmail.com>
2014-07-30 18:05:56 +0800

    28.issue: divide_conquer merge

--- src/28_Binary_Tree_Maximum_Path_Sum.cpp
+++ src/28_Binary_Tree_Maximum_Path_Sum.cpp
@@ -13,30 +13,27 @@ class Solution {
 public:
     int maxPathSum(TreeNode *root) {
 		if (!root)return 0;
-
-		int max_sum=0,left_max=0,right_max=0,left_max_path=0,right_max_path=0;
-		if (root->left) left_max=MaxPath(root->left,left_max_path);
-		if (root->right) right_max=MaxPath(root->right,right_max_path);
-		
-		max_sum=root->val;
-		max_sum+=(left_max_path>right_max_path)?left_max_path:right_max_path;
-		if (right_max<max_sum &&right_max<max_sum)return max_sum;
-		else if (right_max<left_max)return left_max;
-		else return right_max;
-
+		int sum1,sum2;
+		sum1=maxPath(root,sum2);
+		return sum1<sum2?sum2:sum1;
     }
-	int MaxPath(TreeNode *branch,int& max_sum){
+	int maxPath(TreeNode *branch,int& max_sum){
 		if (!branch->left && !branch->right){ 
 			max_sum=branch->val;
 			return max_sum;
 		}
 		int left_max=0,right_max=0,left_max_path=0,right_max_path=0;
-		if (branch->left) left_max=MaxPath(branch->left,left_max_path);
-		if (branch->right) right_max=MaxPath(branch->right,right_max_path);
+		if (branch->left) left_max=maxPath(branch->left,left_max_path);
+		if (branch->right) right_max=maxPath(branch->right,right_max_path);
 		
 		max_sum=branch->val;
 		max_sum+=(left_max_path>right_max_path)?left_max_path:right_max_path;
-		if (right_max<max_sum &&right_max<max_sum)return max_sum;
+		
+		int sum=branch->val;
+		sum+=left_max_path>0?left_max_path:0;
+		sum+=right_max_path>0?right_max_path:0;
+
+		if (right_max<sum &&right_max<sum)return sum;
 		else if (right_max<left_max)return left_max;
 		else return right_max;
 	}
