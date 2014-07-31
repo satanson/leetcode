d7998a77f779be90ff8213d20fb847b27c298a7e
satanson <ranpanf@gmail.com>
2014-07-31 19:00:55 +0800

    76.issue: segmentation fault

--- src/76_Minimum_Window_Substring.cpp
+++ src/76_Minimum_Window_Substring.cpp
@@ -3,22 +3,26 @@ class Solution {
 public:
     string minWindow(string S, string T){
 		int M=S.size(),N=T.size();
-		unordered_map<char,int> m;
-		for(int i=0;i<N;i++)m[T[i]]=i;
-		vector<int> p(N,-1);
-		int i=0,j=M;
-
-		for (int k=0;k<M;k++){
-			if(m.count(S[k])==0)continue;
-			p[m[S[k]]]=k;
-			int i0=*std::min_element(p.begin(),p.end());
-			int j0=*std::max_element(p.begin(),p.end());
-			if (i0==-1 || j0==-1)continue;
-			if (j-i>j0-i0) j=j0,i=i0;
+		if (!M*N) return "";
+		std::fill(inf,inf+sizeof(inf),0);
+		std::fill(sup,sup+sizeof(sup),0);
+		int i=0,j=0;
+		for (i=0;i<N;++i)++inf[T[i]];
+		int count=0;
+		for (i=0;i<M;i++){
+			if (sup[S[i]]<inf[S[i]])++count;
+			++sup[S[i]];
+			if (count==N)break;
 		}
-		
-		return j==M?"":S.substr(i,j+1-i);
+		for (j=0;j<M;j++){
+			if(sup[S[i]]>inf[S[i]])--sup[S[i]];
+			else break;
+		}
+		return i==M?"":S.substr(j,i+1-j);
     }
+private:
+	int inf[256];
+	int sup[256];
 };
 int main(){
 	Solution so;
